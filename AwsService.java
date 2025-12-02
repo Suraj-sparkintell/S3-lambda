79693a8798518ac468c0e1a27814e9c07942a344
skandel1 <suraj.kandel@sparkintell.com>
2025-12-01 18:05:28 -0600

    added handler method

--- AwsService.java
+++ AwsService.java
@@ -1,2 +1,21 @@
-public class AwsService {
+import com.sun.net.httpserver.Request;
+
+import javax.naming.Context;
+import java.util.Collections;
+
+public class AwsService implements Request {
+    @Override
+    public <GatewayResponse> GatewayResponse handleRequest(Object object, Context context) {
+
+        String message = "Hello from TechPrimers";
+        System.out.println(message);
+
+        GatewayResponse response = new GatewayResponse(
+                message,
+                200,
+                Collections.singletonMap("X-Powered-By", "TechPrimers"),
+                false
+        );
+        return response;
+    }
 }
